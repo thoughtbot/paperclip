d76ef9be5812f596f806e3303f91121a78d769a8
jyurek <jyurek@7bbfaf0e-4d1d-0410-9690-a8bb5f8ef2aa>
2007-11-18 20:55:35 +0000

    url is the location for s3
    git-svn-id: https://svn.thoughtbot.com/plugins/paperclip/trunk@248 7bbfaf0e-4d1d-0410-9690-a8bb5f8ef2aa

--- lib/paperclip/storage/s3.rb
+++ lib/paperclip/storage/s3.rb
@@ -30,9 +30,15 @@ module Paperclip
         end
       end
       
+      def url_with_s3 style = nil
+        http_host = definition.s3_host || "http://s3.amazonaws.com"
+        "#{http_host}/#{bucket}/#{url_without_s3(style)}"
+      end
+      alias_method_chain :url, :s3
+      
       def file_name style = nil
         style ||= definition.default_style
-        interpolate( style, definition.path )
+        interpolate( style, definition.url )
       end
       
       def attachment_exists? style = nil
