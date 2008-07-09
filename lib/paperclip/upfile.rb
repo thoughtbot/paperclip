84f1fcbf8d89dbc593ff5953a1e91c4f04194384
Jon Yurek <jyurek@thoughtbot.com>
2008-07-09 17:31:09 -0400

    Update to upfile again.

--- lib/paperclip/upfile.rb
+++ lib/paperclip/upfile.rb
@@ -8,11 +8,12 @@ module Paperclip
     def content_type
       type = (self.path.match(/\.(\w+)$/)[1] rescue "octet-stream").downcase
       case type
-      when %r"jpe?g"                     then "image/jpeg"
-      when %r"png", "gif", "bmp", "tiff" then "image/#{type}"
-      when "txt"                         then "text/plain"
-      when %r"html?"                     then "text/html"
-      when "csv", "xml", "css", "js"     then "text/#{type}"
+      when %r"jpe?g"                 then "image/jpeg"
+      when %r"tiff?"                 then "image/tiff"
+      when %r"png", "gif", "bmp"     then "image/#{type}"
+      when "txt"                     then "text/plain"
+      when %r"html?"                 then "text/html"
+      when "csv", "xml", "css", "js" then "text/#{type}"
       else "application/x-#{type}"
       end
     end
