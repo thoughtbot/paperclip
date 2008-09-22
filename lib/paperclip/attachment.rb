7ee9dfcaf82accad4949e781b723dbf0bfc78edc
Henrik N <henrik@nyh.se>
2008-09-22 12:09:09 +0200

    Saw an issue with a nil content_type sometimes in Safari. Fix by doing to_s.

--- lib/paperclip/attachment.rb
+++ lib/paperclip/attachment.rb
@@ -77,7 +77,7 @@ module Paperclip
       logger.info("[paperclip] Writing attributes for #{name}")
       @queued_for_write[:original]   = uploaded_file.to_tempfile
       instance_write(:file_name,       uploaded_file.original_filename.strip.gsub(/[^\w\d\.\-]+/, '_'))
-      instance_write(:content_type,    uploaded_file.content_type.strip)
+      instance_write(:content_type,    uploaded_file.content_type.to_s.strip)
       instance_write(:file_size,       uploaded_file.size.to_i)
       instance_write(:updated_at,      Time.now)
 
