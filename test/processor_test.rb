3f5516ce882c9562f075065d870273a8fdc49f18
Michał Szajbe <michal.szajbe@gmail.com>
2009-02-06 15:42:44 +0100

    Fixed tests to pass after changes made commited in e655fbe502fbae2b538922b35c6d2f55b1fee3d1. (cherry picked from commit 0ce019185d8258a806179be6453aa4d1220803d6)

--- test/processor_test.rb
+++ test/processor_test.rb
@@ -4,7 +4,7 @@ class ProcessorTest < Test::Unit::TestCase
   should "instantiate and call #make when sent #make to the class" do
     processor = mock
     processor.expects(:make).with()
-    Paperclip::Processor.expects(:new).with(:one, :two).returns(processor)
-    Paperclip::Processor.make(:one, :two)
+    Paperclip::Processor.expects(:new).with(:one, :two, :three).returns(processor)
+    Paperclip::Processor.make(:one, :two, :three)
   end
 end
