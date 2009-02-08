28242594d30995adc63454a1a141020ad0fd06da
Jon Yurek <jyurek@thoughtbot.com>
2009-02-07 21:19:23 -0500

    Content type validation macro to matcher conversion

--- shoulda_macros/matchers.rb
+++ shoulda_macros/matchers.rb
@@ -1,2 +1,3 @@
 require 'shoulda_macros/matchers/have_attached_file_matcher'
 require 'shoulda_macros/matchers/validate_attachment_presence_matcher'
+require 'shoulda_macros/matchers/validate_attachment_content_type_matcher'
