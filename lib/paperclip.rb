121609fab38f2aba5ac3cbe0d6b26fd664ee3090
Jon Yurek <jyurek@thoughtbot.com>
2009-06-18 10:10:25 -0400

    Version 2.2.9.2

--- lib/paperclip.rb
+++ lib/paperclip.rb
@@ -44,7 +44,7 @@ end
 # documentation for Paperclip::ClassMethods for more useful information.
 module Paperclip
 
-  VERSION = "2.2.9.1"
+  VERSION = "2.2.9.2"
 
   class << self
     # Provides configurability to Paperclip. There are a number of options available, such as:
