1bcfc14388d0651c5fc70ab9ca3511144c698903
Jon Yurek <jyurek@thoughtbot.com>
2010-06-08 14:12:07 -0400

    Prevent error coercing nil -> Integer by explicit conversion.

--- lib/paperclip/processor.rb
+++ lib/paperclip/processor.rb
@@ -43,7 +43,7 @@ module Paperclip
     # Replaces Tempfile's +make_tmpname+ with one that honors file extensions.
     def make_tmpname(basename, n)
       extension = File.extname(basename)
-      sprintf("%s,%d,%d%s", File.basename(basename, extension), $$, n, extension)
+      sprintf("%s,%d,%d%s", File.basename(basename, extension), $$, n.to_i, extension)
     end
   end
 end
