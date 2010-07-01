44b02facc3ca42b488b7e52397ea5d307593fa22
Jon Yurek <jyurek@thoughtbot.com>
2010-07-01 09:57:20 -0400

    Don't use #present? since it's not there until 2.2

--- lib/paperclip.rb
+++ lib/paperclip.rb
@@ -350,7 +350,7 @@ module Paperclip
       validation_options = options.dup
       allowed_types = [validation_options[:content_type]].flatten
       validates_each(:"#{name}_content_type", validation_options) do |record, attr, value|
-        if !allowed_types.any?{|t| t === value } && value.present?
+        if !allowed_types.any?{|t| t === value } && !(value.nil? || value.blank?)
           if record.errors.method(:add).arity == -2
             message = options[:message] || "is not one of #{allowed_types.join(", ")}"
             record.errors.add(:"#{name}_content_type", message)
