e600189a78d372f8a6e5adc888ada24983ce0382
reaper <reaper@reaperville.com>
2010-08-31 21:15:47 +0900

    Fixed to_file when using Ruby 1.9.2 and S3.
    Without this you get the fun to trackdown error:
    MyClass failed with Encoding::UndefinedConversionError: "\xFF" from ASCII-8BIT
    to UTF-8

--- lib/paperclip/storage/s3.rb
+++ lib/paperclip/storage/s3.rb
@@ -128,6 +128,7 @@ module Paperclip
       def to_file style = default_style
         return @queued_for_write[style] if @queued_for_write[style]
         file = Tempfile.new(path(style))
+        file.binmode
         file.write(AWS::S3::S3Object.value(path(style), bucket_name))
         file.rewind
         return file
