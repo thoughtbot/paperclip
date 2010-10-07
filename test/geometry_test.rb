b39a77e934ce8171eafca2124c23dae6896f3ff9
Paul Mucur <mudge@mudge.name>
2010-10-07 20:07:40 +0100

    Don't pass blank filenames to identify.
    This addresses an issue where Paperclip can indefinitely block execution
    of an application by telling ImageMagick to "identify -format %wx%h [0]"
    which will then wait for input from STDIN forever.

--- test/geometry_test.rb
+++ test/geometry_test.rb
@@ -120,6 +120,22 @@ class GeometryTest < Test::Unit::TestCase
       assert_raise(Paperclip::NotIdentifiedByImageMagickError){ @geo = Paperclip::Geometry.from_file(file) }
     end
 
+    should "not generate from a blank filename" do
+      file = ""
+      assert_raise(Paperclip::NotIdentifiedByImageMagickError){ @geo = Paperclip::Geometry.from_file(file) }
+    end
+
+    should "not generate from a nil file" do
+      file = nil
+      assert_raise(Paperclip::NotIdentifiedByImageMagickError){ @geo = Paperclip::Geometry.from_file(file) }
+    end
+
+    should "not generate from a file with no path" do
+      file = mock("file", :path => "")
+      file.stubs(:respond_to?).with("path").returns(true)
+      assert_raise(Paperclip::NotIdentifiedByImageMagickError){ @geo = Paperclip::Geometry.from_file(file) }
+    end
+
     should "let us know when a command isn't found versus a processing error" do
       old_path = ENV['PATH']
       begin
