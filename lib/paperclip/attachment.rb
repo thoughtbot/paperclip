16c22840b8792cccba0b8c449c3bd024103fbf1f
Steve Madsen <steve@lightyearsoftware.com>
2010-11-23 00:38:00 -0500

    Properly handle non-UploadedFile types.
    There are other ways to assign to an attachment, and none of them act like
    ActionDispatch::Http::UploadedFile, requiring use of the tempfile delegate.

--- lib/paperclip/attachment.rb
+++ lib/paperclip/attachment.rb
@@ -93,7 +93,11 @@ module Paperclip
       instance_write(:file_name,       uploaded_file.original_filename.strip)
       instance_write(:content_type,    uploaded_file.content_type.to_s.strip)
       instance_write(:file_size,       uploaded_file.size.to_i)
-      instance_write(:fingerprint,     uploaded_file.tempfile.fingerprint)
+      if uploaded_file.is_a?(ActionDispatch::Http::UploadedFile)
+        instance_write(:fingerprint,   uploaded_file.tempfile.fingerprint)
+      else
+        instance_write(:fingerprint,   uploaded_file.fingerprint)
+      end
       instance_write(:updated_at,      Time.now)
 
       @dirty = true
