5c85095e98bf9b101145d4a18af750502e7c81ed
Prem Sichanugrist <s@sikachu.com>
2011-09-03 15:06:51 +0700

    Default the style to default style in #s3_protocol method of S3 storage
    Thank you @eric-hu for reporting this regression.
    
    Closes #583

--- lib/paperclip/storage/s3.rb
+++ lib/paperclip/storage/s3.rb
@@ -153,7 +153,7 @@ module Paperclip
         end
       end
 
-      def s3_protocol(style)
+      def s3_protocol(style = default_style)
         if @s3_protocol.is_a?(Proc)
           @s3_protocol.call(style)
         else
