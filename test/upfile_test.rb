cb6efaa5861f9f83f8232fed2da21edc5173bd4e
Prem Sichanugrist <s@sikachu.com>
2011-09-03 23:19:57 +0700

    Remove duplicate test case

--- test/upfile_test.rb
+++ test/upfile_test.rb
@@ -24,14 +24,6 @@ class UpfileTest < Test::Unit::TestCase
 
         assert_equal content_type, file.content_type
       end
-
-      should "return a content_type of text/plain on a real file whose content_type is determined with the file command" do
-        file = File.new(File.join(File.dirname(__FILE__), "..", "LICENSE"))
-        class << file
-          include Paperclip::Upfile
-        end
-        assert_equal 'text/plain', file.content_type
-      end
     end
   end
 
