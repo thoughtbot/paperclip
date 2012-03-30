e10edcda67d552a723826961acd57752a54e2aa4
Jon Yurek <jyurek@thoughtbot.com>
2012-03-30 14:21:06 -0400

    Remove unnecessary step

--- features/step_definitions/rails_steps.rb
+++ features/step_definitions/rails_steps.rb
@@ -1,7 +1,3 @@
-When /^I print "([^\"]*)"$/ do |whatever|
-  puts whatever
-end
-
 Given /^I generate a new rails application$/ do
   steps %{
     When I run `bundle exec #{new_application_command} #{APP_NAME}`
