87f425605207b6398b53ac3d58664d7d46d3139c
Steve Cosman <scosman@gmail.com>
2012-04-13 11:57:03 -0400

    Make the adapters register more explicit to ensure we always work when it triggers

--- lib/paperclip/io_adapters/sinatra_file_adapter.rb
+++ lib/paperclip/io_adapters/sinatra_file_adapter.rb
@@ -46,5 +46,5 @@ module Paperclip
 end
 
 Paperclip.io_adapters.register Paperclip::SinatraFileAdapter do |target|
-  target.class == Hash && !target[:tempfile].nil?
+  target.class == Hash && !target[:tempfile].nil? && (File === target[:tempfile] || Tempfile === target[:tempfile]) 
 end
