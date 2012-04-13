018893a06910cfe2a820013e19c4eb165c585d8d
Steve Cosman <scosman@gmail.com>
2012-04-13 11:29:53 -0400

    Add a file adapter for sinatra

---
+++ lib/paperclip/io_adapters/sinatra_file_adapter.rb
@@ -0,0 +1,50 @@
+module Paperclip
+  class SinatraFileAdapter
+    def initialize(target)
+      @target = target
+
+      @tempfile = @target[:tempfile]
+    end
+
+    def original_filename
+      @target[:filename]
+    end
+
+    def content_type
+      @target[:type]
+    end
+
+    def fingerprint
+      @fingerprint ||= Digest::MD5.file(path).to_s
+    end
+
+    def size
+      File.size(path)
+    end
+
+    def nil?
+      false
+    end
+
+    def read(length = nil, buffer = nil)
+      @tempfile.read(length, buffer)
+    end
+
+    # We don't use this directly, but aws/sdk does.
+    def rewind
+      @tempfile.rewind
+    end
+
+    def eof?
+      @tempfile.eof?
+    end
+
+    def path
+      @tempfile.path
+    end
+  end
+end
+
+Paperclip.io_adapters.register Paperclip::SinatraFileAdapter do |target|
+  target.class == Hash && !target[:tempfile].nil?
+end
