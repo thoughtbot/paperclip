43f5d4f8e39d814496655cc007cdaa5002df77e9
Mark Kremer <mark@without-brains.net>
2012-04-14 09:41:34 +0200

    Added a basic cucumber feature that shows that the Railtie configuration works

--- features/step_definitions/rails_steps.rb
+++ features/step_definitions/rails_steps.rb
@@ -65,6 +65,14 @@ Given /^I add this snippet to the User model:$/ do |snippet|
   end
 end
 
+Given /^I add this snippet to config\/application.rb:$/ do |snippet|
+  file_name = "config/application.rb"
+  in_current_dir do
+    content = File.read(file_name)
+    File.open(file_name, 'w') {|f| f << content.sub(/class Application < Rails::Application.*$/, "class Application < Rails::Application\n#{snippet}\n")}
+  end
+end
+
 Given /^I start the rails application$/ do
   in_current_dir do
     require "./config/environment"
