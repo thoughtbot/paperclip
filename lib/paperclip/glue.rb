2860c701763d436de678c2c047bd93cb09c4fa45
Prem Sichanugrist <s@sikachu.com>
2012-06-16 14:46:25 -0400

    Require Paperclip::Schema only when used with AR
    This will solve an issue when using Paperclip without Active Record
    such as Mongoid.
    
    Fixes #919

--- lib/paperclip/glue.rb
+++ lib/paperclip/glue.rb
@@ -8,14 +8,8 @@ module Paperclip
       base.extend ClassMethods
       base.send :include, Callbacks
       base.send :include, Validators
-      base.send :include, Schema
+      base.send :include, Schema if defined? ActiveRecord
       base.class_attribute :attachment_definitions
-
-      if defined?(ActiveRecord)
-        ActiveRecord::ConnectionAdapters::AbstractAdapter.send(:include, Paperclip::Schema)
-        ActiveRecord::ConnectionAdapters::Table.send(:include, Paperclip::Schema)
-        ActiveRecord::ConnectionAdapters::TableDefinition.send(:include, Paperclip::Schema)
-      end
 
       locale_path = Dir.glob(File.dirname(__FILE__) + "/locales/*.{rb,yml}")
       I18n.load_path += locale_path unless I18n.load_path.include?(locale_path)
