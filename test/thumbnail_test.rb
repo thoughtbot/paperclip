185174976ac85438f7f9a93a1c3400df6c386d58
Jon Yurek <jyurek@thoughtbot.com>
2012-11-07 21:47:31 -0500

    Ensure commands can't be found in tests

--- test/thumbnail_test.rb
+++ test/thumbnail_test.rb
@@ -76,6 +76,8 @@ class ThumbnailTest < Test::Unit::TestCase
       should "let us know when a command isn't found versus a processing error" do
         old_path = ENV['PATH']
         begin
+          Cocaine::CommandLine.path = ''
+          Paperclip.options[:command_path] = ''
           ENV['PATH'] = ''
           assert_raises(Paperclip::CommandNotFoundError) do
             @thumb.make
@@ -202,6 +204,8 @@ class ThumbnailTest < Test::Unit::TestCase
         should "let us know when a command isn't found versus a processing error" do
           old_path = ENV['PATH']
           begin
+            Cocaine::CommandLine.path = ''
+            Paperclip.options[:command_path] = ''
             ENV['PATH'] = ''
             assert_raises(Paperclip::CommandNotFoundError) do
               @thumb.make
