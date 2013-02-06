7fea4247e93bf360847f2ba0b6d474c9a3acbcca
Les Hill <leshill@gmail.com>
2013-02-05 16:15:21 -0800

    Refactor style specific headers code
    Allow either a hash or a Style object to set the headers.

--- lib/paperclip/storage/s3.rb
+++ lib/paperclip/storage/s3.rb
@@ -311,9 +311,10 @@ module Paperclip
             end
 
             style_specific_options = styles[style]
-            if style_specific_options.is_a?(Hash)
-              merge_s3_headers( style_specific_options[:s3_headers], @s3_headers, @s3_metadata) if style_specific_options.has_key?(:s3_headers)
-              @s3_metadata.merge!(style_specific_options[:s3_metadata]) if style_specific_options.has_key?(:s3_metadata)
+
+            if style_specific_options
+              merge_s3_headers( style_specific_options[:s3_headers], @s3_headers, @s3_metadata) if style_specific_options[:s3_headers]
+              @s3_metadata.merge!(style_specific_options[:s3_metadata]) if style_specific_options[:s3_metadata]
             end
 
             write_options[:metadata] = @s3_metadata unless @s3_metadata.empty?
