6b04436517f0b38ddc57d4a3ab7d4e0e2aec5a41
Scott Carleton <scott@artsicle.com>
2013-08-29 12:29:23 -0400

    failing spec for attachment definitions

--- test/attachment_registry_test.rb
+++ test/attachment_registry_test.rb
@@ -62,6 +62,20 @@ class AttachmentRegistryTest < Test::Unit::TestCase
 
       assert_equal expected_definitions, definitions
     end
+
+    should "produce defintions for subclasses" do
+      expected_definitions = {
+        avatar: { yo: 'greeting' }
+      }
+      foo = Class.new
+      bar = Class.new(foo)
+
+      Paperclip::AttachmentRegistry.register(foo, :avatar, { yo: 'greeting' })
+
+      definitions = Paperclip::AttachmentRegistry.definitions_for(bar)
+
+      assert_equal expected_definitions, definitions
+    end
   end
 
   context '.clear' do
