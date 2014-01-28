fe19916633fcd10d5429066bc60cfb2ca5992e22
Adam Niedzielski <adamsunday@gmail.com>
2014-01-28 21:13:18 +0100

    Memoize result of identify command in order to avoid multiple calls to ImageMagick

--- lib/paperclip/thumbnail.rb
+++ lib/paperclip/thumbnail.rb
@@ -109,7 +109,10 @@ module Paperclip
 
     # Return true if ImageMagick's +identify+ returns an animated format
     def identified_as_animated?
-      ANIMATED_FORMATS.include? identify("-format %m :file", :file => "#{@file.path}[0]").to_s.downcase.strip
+      if @identified_as_animated.nil?
+        @identified_as_animated = ANIMATED_FORMATS.include? identify("-format %m :file", :file => "#{@file.path}[0]").to_s.downcase.strip
+      end
+      @identified_as_animated
     rescue Cocaine::ExitStatusError => e
       raise Paperclip::Error, "There was an error running `identify` for #{@basename}" if @whiny
     rescue Cocaine::CommandNotFoundError => e
