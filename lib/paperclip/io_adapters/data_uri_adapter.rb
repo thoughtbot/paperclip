44c10cc1993edbe04e84d7b9221536beaec37428
Marco Otte-Witte <mottewitte@rim.com>
2014-01-28 14:09:12 +0100

    make mime type optional in data URIs

--- lib/paperclip/io_adapters/data_uri_adapter.rb
+++ lib/paperclip/io_adapters/data_uri_adapter.rb
@@ -1,7 +1,7 @@
 module Paperclip
   class DataUriAdapter < StringioAdapter
 
-    REGEXP = /\Adata:([-\w]+\/[-\w\+]+);base64,(.*)/m
+    REGEXP = /\Adata:([-\w]+\/[-\w\+]+)?;base64,(.*)/m
 
     def initialize(target_uri)
       super(extract_target(target_uri))
