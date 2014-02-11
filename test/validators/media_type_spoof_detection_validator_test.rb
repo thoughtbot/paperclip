00787da621735b8681ab035a78b85b4412f0dc5c
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2014-02-11 12:29:04 +0100

    add default translation for spoofed media type error

--- test/validators/media_type_spoof_detection_validator_test.rb
+++ test/validators/media_type_spoof_detection_validator_test.rb
@@ -6,7 +6,27 @@ class MediaTypeSpoofDetectionValidatorTest < Test::Unit::TestCase
     @dummy = Dummy.new
   end
 
+  def build_validator(options = {})
+    @validator = Paperclip::Validators::MediaTypeSpoofDetectionValidator.new(options.merge(
+      :attributes => :avatar
+    ))
+  end
+
   should "be on the attachment without being explicitly added" do
     assert Dummy.validators_on(:avatar).any?{ |validator| validator.kind == :media_type_spoof_detection }
+  end
+
+  should "return default error message for spoofed media type" do
+    build_validator
+
+    # Make avatar dirty
+    file = File.new(fixture_file("5k.png"), "rb")
+    @dummy.avatar.assign(file)
+
+    detector = mock("detector", :spoofed? => true)
+    Paperclip::MediaTypeSpoofDetector.stubs(:using).returns(detector)
+    @validator.validate(@dummy)
+
+    assert_equal "media type is spoofed", @dummy.errors[:avatar].first
   end
 end
