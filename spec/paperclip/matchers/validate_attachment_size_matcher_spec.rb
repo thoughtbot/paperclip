dc256ffa2d92a7edcb22c7ec9428d8ed4a4845d9
Jon Yurek <jyurek@thoughtbot.com>
2014-03-05 10:39:54 -0500

    Put methods from spec/spec_helper in spec/support
    There were a bunch of free-floating methods in spec_helper. They were
    pulled over from test/helper, but they shouldn't be there. Move them
    into appropriate modules in spec/support. This commit also cleans up the
    "should_accept/reject_dummy_class methods in favor of using proper
    matchers directly. This commit should probably not get squashed or git
    will likely forget that those matcher specs are the same file because of
    how much changed.

--- spec/paperclip/matchers/validate_attachment_size_matcher_spec.rb
+++ spec/paperclip/matchers/validate_attachment_size_matcher_spec.rb
@@ -4,91 +4,85 @@ require 'paperclip/matchers'
 describe Paperclip::Shoulda::Matchers::ValidateAttachmentSizeMatcher do
   extend Paperclip::Shoulda::Matchers
 
-  context "validate_attachment_size" do
+  before do
+    reset_table("dummies") do |d|
+      d.string :avatar_file_name
+      d.integer :avatar_file_size
+    end
+    reset_class "Dummy"
+    Dummy.do_not_validate_attachment_file_type :avatar
+    Dummy.has_attached_file :avatar
+  end
+
+  context "Limiting size" do
+    it "rejects a class with no validation" do
+      expect(matcher.in(256..1024)).to_not accept(Dummy)
+    end
+
+    it "rejects a class with a validation that's too high" do
+      Dummy.validates_attachment_size :avatar, in: 256..2048
+      expect(matcher.in(256..1024)).to_not accept(Dummy)
+    end
+
+    it "accepts a class with a validation that's too low" do
+      Dummy.validates_attachment_size :avatar, in: 0..1024
+      expect(matcher.in(256..1024)).to_not accept(Dummy)
+    end
+
+    it "accepts a class with a validation that matches" do
+      Dummy.validates_attachment_size :avatar, in: 256..1024
+      expect(matcher.in(256..1024)).to accept(Dummy)
+    end
+  end
+
+  context "allowing anything" do
+    it "given a class with an upper limit" do
+      Dummy.validates_attachment_size :avatar, less_than: 1
+      expect(matcher).to accept(Dummy)
+    end
+
+    it "given a class with a lower limit" do
+      Dummy.validates_attachment_size :avatar, greater_than: 1
+      expect(matcher).to accept(Dummy)
+    end
+  end
+
+  context "using an :if to control the validation" do
     before do
-      reset_table("dummies") do |d|
-        d.string :avatar_file_name
-        d.integer :avatar_file_size
-      end
-      reset_class "Dummy"
-      Dummy.do_not_validate_attachment_file_type :avatar
-      Dummy.has_attached_file :avatar
-    end
-
-    context "of limited size" do
-      before do
-        @matcher = self.class.validate_attachment_size(:avatar).in(256..1024)
-      end
-
-      context "given a class with no validation" do
-        should_reject_dummy_class
-      end
-
-      context "given a class with a validation that's too high" do
-        before { Dummy.validates_attachment_size :avatar, in: 256..2048 }
-        should_reject_dummy_class
-      end
-
-      context "given a class with a validation that's too low" do
-        before { Dummy.validates_attachment_size :avatar, in: 0..1024 }
-        should_reject_dummy_class
-      end
-
-      context "given a class with a validation that matches" do
-        before { Dummy.validates_attachment_size :avatar, in: 256..1024 }
-        should_accept_dummy_class
+      Dummy.class_eval do
+        validates_attachment_size :avatar, greater_than: 1024, if: :go
+        attr_accessor :go
       end
     end
 
-    context "allowing anything" do
-      before do
-        @matcher = self.class.validate_attachment_size(:avatar)
-      end
-
-      context "given a class with an upper limit" do
-        before { Dummy.validates_attachment_size :avatar, less_than: 1 }
-        should_accept_dummy_class
-      end
-
-      context "given a class with a lower limit" do
-        before { Dummy.validates_attachment_size :avatar, greater_than: 1 }
-        should_accept_dummy_class
-      end
+    it "run the validation if the control is true" do
+      dummy = Dummy.new
+      dummy.go = true
+      expect(matcher.greater_than(1024)).to accept(dummy)
     end
 
-    context "using an :if to control the validation" do
-      before do
-        Dummy.class_eval do
-          validates_attachment_size :avatar, greater_than: 1024, if: :go
-          attr_accessor :go
-        end
-        @dummy = Dummy.new
-        @matcher = self.class.validate_attachment_size(:avatar).greater_than(1024)
-      end
+    it "not run the validation if the control is false" do
+      dummy = Dummy.new
+      dummy.go = false
+      expect(matcher.greater_than(1024)).to_not accept(dummy)
+    end
+  end
 
-      it "run the validation if the control is true" do
-        @dummy.go = true
-        expect(@matcher).to accept(@dummy)
-      end
-
-      it "not run the validation if the control is false" do
-        @dummy.go = false
-        expect(@matcher).to_not accept(@dummy)
-      end
+  context "post processing" do
+    before do
+      Dummy.validates_attachment_size :avatar, greater_than: 1024
     end
 
-    context "post processing" do
-      before do
-        Dummy.validates_attachment_size :avatar, greater_than: 1024
-
-        @dummy = Dummy.new
-        @matcher = self.class.validate_attachment_size(:avatar).greater_than(1024)
-      end
-
-      it "be skipped" do
-        @dummy.avatar.expects(:post_process).never
-        expect(@matcher).to accept(@dummy)
-      end
+    it "be skipped" do
+      dummy = Dummy.new
+      dummy.avatar.expects(:post_process).never
+      expect(matcher.greater_than(1024)).to accept(dummy)
     end
+  end
+
+  private
+
+  def matcher
+    self.class.validate_attachment_size(:avatar)
   end
 end
