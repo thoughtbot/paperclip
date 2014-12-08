b589d5a15648347cf4b3b354d5b9cce44171644d
Jon Yurek <jyurek@thoughtbot.com>
2014-12-08 15:37:14 -0500

    Add Rails 4.2 to Appraisals

--- spec/support/matchers/have_column.rb
+++ spec/support/matchers/have_column.rb
@@ -5,10 +5,10 @@ RSpec::Matchers.define :have_column do |column_name|
 
   match do |columns|
     column = columns.detect{|column| column.name == column_name }
-    column && column.default == @default
+    column && column.default.to_s == @default.to_s
   end
 
   failure_message_for_should do |columns|
-    "expected to find #{column_name}, default #{@default} in #{columns.map{|column| [column.name, column.default] }}"
+    "expected to find '#{column_name}', default '#{@default}' in #{columns.map{|column| [column.name, column.default] }}"
   end
 end
