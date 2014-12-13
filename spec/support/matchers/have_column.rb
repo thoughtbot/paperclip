3fbda79f4c289f04afc21c3985ddd6077c25623c
soramugi <soramugi.chika@gmail.com>
2014-12-13 11:14:38 +0900

    Rspec 3 Use `failure_message`
    Deprecation Warnings:
    
    ```
    `failure_message_for_should` is deprecated. Use `failure_message` instead. Called from /Users/mugi/git/paperclip/spec/support/matchers/have_column.rb:11:in `block in <top (required)>'.
    ```

--- spec/support/matchers/have_column.rb
+++ spec/support/matchers/have_column.rb
@@ -8,7 +8,7 @@ RSpec::Matchers.define :have_column do |column_name|
     column && column.default.to_s == @default.to_s
   end
 
-  failure_message_for_should do |columns|
+  failure_message do |columns|
     "expected to find '#{column_name}', default '#{@default}' in #{columns.map{|column| [column.name, column.default] }}"
   end
 end
