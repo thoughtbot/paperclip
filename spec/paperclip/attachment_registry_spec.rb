0f8f8e79f46c94178e8f09c4051cffdc02cdf2d5
Agustin Cavilliotti <cavi21@gmail.com>
2015-01-30 18:46:24 -0300

    Arrangements in the spec file to take care of some @houndci comments

--- spec/paperclip/attachment_registry_spec.rb
+++ spec/paperclip/attachment_registry_spec.rb
@@ -54,38 +54,48 @@ describe 'Attachment Registry' do
         greeter: { ciao: 'greeting' }
       }
       foo = Class.new
-      Paperclip::AttachmentRegistry.register(foo, :avatar, { yo: 'greeting' })
-      Paperclip::AttachmentRegistry.register(foo, :greeter, { ciao: 'greeting' })
+      Paperclip::AttachmentRegistry.register(foo,
+                                             :avatar,
+                                             { yo: 'greeting' })
+      Paperclip::AttachmentRegistry.register(foo,
+                                             :greeter,
+                                             { ciao: 'greeting' })
 
       definitions = Paperclip::AttachmentRegistry.definitions_for(foo)
 
       assert_equal expected_definitions, definitions
     end
 
-    it "produces defintions for subclasses" do
+    it 'produces defintions for subclasses' do
       expected_definitions = { avatar: { yo: 'greeting' } }
       foo = Class.new
       bar = Class.new(foo)
-      Paperclip::AttachmentRegistry.register(foo, :avatar, expected_definitions[:avatar])
+      Paperclip::AttachmentRegistry.register(foo,
+                                             :avatar,
+                                             expected_definitions[:avatar])
 
       definitions = Paperclip::AttachmentRegistry.definitions_for(bar)
 
       assert_equal expected_definitions, definitions
     end
 
-    it "produces defintions for subclasses but deep merging them" do
+    it 'produces defintions for subclasses but deep merging them' do
       foo_definitions = { avatar: { yo: 'greeting' } }
       bar_definitions = { avatar: { ciao: 'greeting' } }
       expected_definitions = {
         avatar: {
           yo: 'greeting',
           ciao: 'greeting'
-         }
+        }
       }
       foo = Class.new
       bar = Class.new(foo)
-      Paperclip::AttachmentRegistry.register(foo, :avatar, foo_definitions[:avatar])
-      Paperclip::AttachmentRegistry.register(bar, :avatar, bar_definitions[:avatar])
+      Paperclip::AttachmentRegistry.register(foo,
+                                             :avatar,
+                                             foo_definitions[:avatar])
+      Paperclip::AttachmentRegistry.register(bar,
+                                             :avatar,
+                                             bar_definitions[:avatar])
 
       definitions = Paperclip::AttachmentRegistry.definitions_for(bar)
 
@@ -96,7 +106,9 @@ describe 'Attachment Registry' do
   context '.clear' do
     it 'removes all of the existing attachment definitions' do
       foo = Class.new
-      Paperclip::AttachmentRegistry.register(foo, :greeter, { ciao: 'greeting' })
+      Paperclip::AttachmentRegistry.register(foo,
+                                             :greeter,
+                                             { ciao: 'greeting' })
 
       Paperclip::AttachmentRegistry.clear
 
