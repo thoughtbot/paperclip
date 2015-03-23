ece46f46e9b9c327ee65402f6510557f1fca2ed1
Arjun Anand <arjun.anand84@gmail.com>
2015-03-23 15:22:34 -0400

    styling tweaks

--- lib/paperclip/has_attached_file.rb
+++ lib/paperclip/has_attached_file.rb
@@ -92,7 +92,9 @@ module Paperclip
       @klass.send(:after_save) { send(name).send(:save) }
       @klass.send(:before_destroy) { send(name).send(:queue_all_for_delete) }
       if @klass.respond_to?(:after_commit)
-        @klass.send(:after_commit, :on => :destroy) { send(name).send(:flush_deletes) }
+        @klass.send(:after_commit, on: :destroy) do
+          send(name).send(:flush_deletes)
+        end
       else
         @klass.send(:after_destroy) { send(name).send(:flush_deletes) }
       end
