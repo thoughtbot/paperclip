4b2eebdd9bfb65f7d0f980e573ac65fe92ab146b
Isaac Betesh <iybetesh@gmail.com>
2015-06-17 16:52:02 -0400

    Cucumber needs to tell FakeWeb the s3_domain_url path for uploading a file, and FakeWeb needs to return XML

--- features/step_definitions/s3_steps.rb
+++ features/step_definitions/s3_steps.rb
@@ -1,11 +1,15 @@
 When /^I attach the file "([^"]*)" to "([^"]*)" on S3$/ do |file_path, field|
   definition = Paperclip::AttachmentRegistry.definitions_for(User)[field.downcase.to_sym]
-  path = "https://paperclip.s3.amazonaws.com#{definition[:path]}"
+  path = if defined?(::AWS)
+    "https://paperclip.s3.amazonaws.com#{definition[:path]}"
+  else
+    "https://paperclip.s3-us-west-2.amazonaws.com#{definition[:path]}"
+  end
   path.gsub!(':filename', File.basename(file_path))
   path.gsub!(/:([^\/\.]+)/) do |match|
     "([^\/\.]+)"
   end
-  FakeWeb.register_uri(:put, Regexp.new(path), :body => "OK")
+  FakeWeb.register_uri(:put, Regexp.new(path), :body => defined?(::AWS) ? "OK" : "<xml></xml>")
   step "I attach the file \"#{file_path}\" to \"#{field}\""
 end
 
