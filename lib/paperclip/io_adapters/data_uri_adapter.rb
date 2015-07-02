57a8cbf2fd5a317b7af2e74e07aafb8e27501e86
David Rivera <drivera@weebly.com>
2015-07-01 21:16:20 -0700

    can now handle data svg

--- lib/paperclip/io_adapters/data_uri_adapter.rb
+++ lib/paperclip/io_adapters/data_uri_adapter.rb
@@ -1,7 +1,9 @@
 module Paperclip
   class DataUriAdapter < StringioAdapter
 
-    REGEXP = /\Adata:([-\w]+\/[-\w\+\.]+)?;base64,(.*)/m
+    REGEXP = /\Adata:([-\w]+\/[-\w\+\.]+)?;(base64|utf8),(.*)/m
+
+    XML_TAG = %{<?xml version="1.0" standalone="yes"?>}
 
     def initialize(target_uri)
       super(extract_target(target_uri))
@@ -11,9 +13,32 @@ module Paperclip
 
     def extract_target(uri)
       data_uri_parts = uri.match(REGEXP) || []
-      StringIO.new(Base64.decode64(data_uri_parts[2] || ''))
+      decoded = get_image_from_match(data_uri_parts)
+
+      if is_svg_image?(decoded)
+        decoded = generate_xml_string(decoded)
+      end
+
+      StringIO.new(decoded)
     end
 
+    def is_svg_image?(img)
+      img.start_with?('<svg')
+    end
+
+    def get_image_from_match(match)
+      return '' if match.length == 0
+
+      if match[2].eql?('base64')
+        return Base64.decode64(match[3] || '')
+      end
+
+      match[3] || ''
+    end
+
+    def generate_xml_string(svg)
+      [XML_TAG, svg].flatten.join("\n")
+    end
   end
 end
 
