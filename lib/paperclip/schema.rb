d331f3a7f29817e28f4bd591eb52d0ff5983175f
Andrew Hubbs <andrew@hub.bs>
2015-07-27 10:09:10 -0700

    Fix remove column call to work in Rails 3 and 4
    Removes `column_type` and `column_options` so that it works well in both
    versions.
    
    Backcported from master, to release a patch version with this bugfix.
    
    [closes #1947]

--- lib/paperclip/schema.rb
+++ lib/paperclip/schema.rb
@@ -40,7 +40,7 @@ module Paperclip
         attachment_names.each do |attachment_name|
           COLUMNS.each_pair do |column_name, column_type|
             column_options = options.merge(options[column_name.to_sym] || {})
-            remove_column(table_name, "#{attachment_name}_#{column_name}", column_type, column_options)
+            remove_column(table_name, "#{attachment_name}_#{column_name}")
           end
         end
       end
