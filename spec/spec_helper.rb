85d9af493ab40309c12a5ad17f62d57c8b8876f2
kevcha <kevin@craftsmen.io>
2016-01-26 11:24:22 +0100

    Remove attr_protected related specs (was removed in rails 4)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,7 +37,6 @@ RSpec.configure do |config|
   config.include ModelReconstruction
   config.include TestData
   config.extend VersionHelper
-  config.extend RailsHelpers::ClassMethods
   config.mock_framework = :mocha
   config.before(:all) do
     rebuild_model
--- spec/support/rails_helpers.rb
+++
@@ -1,7 +0,0 @@
-module RailsHelpers
-  module ClassMethods
-    def using_protected_attributes?
-      ActiveRecord::VERSION::MAJOR < 4
-    end
-  end
-end
