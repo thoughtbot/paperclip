73b236e7139b430d70eeb603c43efafa569588a5
Jon Moss <me@jonathanmoss.me>
2016-02-08 16:35:26 -0500

    Remove deprecations

--- spec/support/deprecations.rb
+++
@@ -1,9 +0,0 @@
-RSpec.configure do |config|
-  config.before(:all) do
-    ActiveSupport::Deprecation.silenced = true
-  end
-  config.before(:each) do
-    Paperclip::Deprecations.stubs(:active_model_version).returns("4.2")
-    Paperclip::Deprecations.stubs(:aws_sdk_version).returns("2.0.0")
-  end
-end
