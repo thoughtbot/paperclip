5d8dd457c64e75c19349445f60d6c16bea99b9e7
Jacob Bullock <jacob.bullock@gmail.com>
2016-03-30 19:43:47 -0600

    spread convert call to mutliple lines to minimize line length

--- lib/paperclip/thumbnail.rb
+++ lib/paperclip/thumbnail.rb
@@ -77,7 +77,11 @@ module Paperclip
 
         parameters = parameters.flatten.compact.join(" ").strip.squeeze(" ")
 
-        success = convert(parameters, :source => "#{File.expand_path(src.path)}#{'[' + @frame_index.to_s + ']' unless animated?}", :dest => File.expand_path(dst.path))
+        success = convert(parameters, 
+          :source => "#{File.expand_path(src.path)}#{'[' + @frame_index.to_s + ']' unless animated?}", 
+          :dest => File.expand_path(dst.path)
+          )
+          
       rescue Cocaine::ExitStatusError => e
         raise Paperclip::Error, "There was an error processing the thumbnail for #{@basename}" if @whiny
       rescue Cocaine::CommandNotFoundError => e
