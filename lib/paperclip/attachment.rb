2dcb7fd99a04062e8f4758bbf5a48217982abc0c
Cameron Finucane <cameron@tomyumcorp.com>
2016-05-05 18:09:12 -0400

    Clarify semantics of the only_process option (documentation-only)
    This was really confusing to me, and I think this small change would help to clarify what's going on without having to trace through the code.

--- lib/paperclip/attachment.rb
+++ lib/paperclip/attachment.rb
@@ -50,7 +50,8 @@ module Paperclip
     # +url+ - a relative URL of the attachment. This is interpolated using +interpolator+
     # +path+ - where on the filesystem to store the attachment. This is interpolated using +interpolator+
     # +styles+ - a hash of options for processing the attachment. See +has_attached_file+ for the details
-    # +only_process+ - style args to be run through the post-processor. This defaults to the empty list
+    # +only_process+ - style args to be run through the post-processor. This defaults to the empty list (which is 
+    #                  a special case that indicates all styles should be processed)
     # +default_url+ - a URL for the missing image
     # +default_style+ - the style to use when an argument is not specified e.g. #url, #path
     # +storage+ - the storage mechanism. Defaults to :filesystem
