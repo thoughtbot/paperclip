2c59f96fe4ce8346a7d9bb9a062f2cd7073d035a
Tute Costa <tutecosta@gmail.com>
2016-05-10 17:51:43 -0400

    Automatically close file while checking mime type
    [fixes #2016]

--- lib/paperclip/content_type_detector.rb
+++ lib/paperclip/content_type_detector.rb
@@ -67,8 +67,9 @@ module Paperclip
     end
 
     def type_from_mime_magic
-      @type_from_mime_magic ||=
-        MimeMagic.by_magic(File.open(@filepath)).try(:type)
+      @type_from_mime_magic ||= File.open(@filepath) do |file|
+        MimeMagic.by_magic(file).try(:type)
+      end
     end
 
     def type_from_file_command
