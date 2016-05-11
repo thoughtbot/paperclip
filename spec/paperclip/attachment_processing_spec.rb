23d2ecb4d3b7691cfb6153b3d436eb36c33702e5
Jonathan Rochkind <jonathan@friendsoftheweb.com>
2016-05-11 14:35:28 -0400

    remove tentative specs that never worked, replaced with separate file

--- spec/paperclip/attachment_processing_spec.rb
+++ spec/paperclip/attachment_processing_spec.rb
@@ -44,40 +44,6 @@ describe 'Attachment Processing' do
 
       attachment.assign(invalid_assignment)
     end
-
-    context "with custom post-processing" do
-      before do
-        Dummy.class_eval do
-          validates_attachment_content_type :avatar, content_type: "image/png"
-          before_avatar_post_process :do_before_avatar
-          after_avatar_post_process :do_after_avatar
-          before_post_process :do_before_all
-          after_post_process :do_after_all
-          def do_before_avatar; end
-
-          def do_after_avatar; end
-
-          def do_before_all; end
-
-          def do_after_all; end
-        end
-      end
-      ## FALSE POSITIVE: This passes even before our change in callbacks.rb,
-      # even though we know it had a problem with this. This passes
-      # even if we don't trigger a validation error, we haven't succesfully
-      # set up our callbacks at all somehow.
-      it "does not run custom post-processing if validation fails" do
-        file = File.new(fixture_file("5k.png"))
-
-        instance = Dummy.new
-        attachment = instance.avatar
-
-        attachment.expects(:do_after_avatar).never
-        attachment.expects(:do_after_all).never
-
-        attachment.assign(file)
-      end
-    end
   end
 
   context 'using validates_attachment' do
