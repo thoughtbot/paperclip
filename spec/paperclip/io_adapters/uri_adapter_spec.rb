431d1e8bfcf2c9a49bf3a4db2725684fc377d7e8
Sergey Novikov <droptheplot@users.noreply.github.com>
2016-06-16 21:46:01 +0300

    Add read_timeout option to UriAdapter download_content method (#2232)
    * Add `read_timeout` option to `UriAdapter#download_content` method
    * Default `read_timeout` is `nil`
    * Update NEWS

--- spec/paperclip/io_adapters/uri_adapter_spec.rb
+++ spec/paperclip/io_adapters/uri_adapter_spec.rb
@@ -99,4 +99,31 @@ describe Paperclip::UriAdapter do
     end
   end
 
+  describe "#download_content" do
+    before do
+      Paperclip::UriAdapter.any_instance.stubs(:open).returns(StringIO.new("xxx"))
+      @uri = URI.parse("https://github.com/thoughtbot/paper:clip.jpg")
+      @subject = Paperclip.io_adapters.for(@uri)
+    end
+
+    after do
+      @subject.send(:download_content)
+    end
+
+    context "with default read_timeout" do
+      it "calls open without options" do
+        @subject.expects(:open).with(@uri, {}).at_least_once
+      end
+    end
+
+    context "with custom read_timeout" do
+      before do
+        Paperclip.options[:read_timeout] = 120
+      end
+
+      it "calls open with read_timeout option" do
+        @subject.expects(:open).with(@uri, read_timeout: 120).at_least_once
+      end
+    end
+  end
 end
