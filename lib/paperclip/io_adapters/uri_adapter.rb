e367efc2a44d8f137aad35facd9d4f61749204af
Jonathan Garay <jonathan@devmask.net>
2016-07-23 15:55:07 -0500

    The uri io adapter should use the content-disposition filename (#2250)
    - The uri io adapter now seeks for the content-disposition header
      if this is pressent the value filename is taken instead of the
      last path segment for the resource file name
    - Fix style comments
    - Applied the Tute Costa refactor to URI Adapter.
    - Added entry to  the NEWS file.
    - Removed editor tracking file
    - Fix test cases

--- lib/paperclip/io_adapters/uri_adapter.rb
+++ lib/paperclip/io_adapters/uri_adapter.rb
@@ -2,6 +2,8 @@ require 'open-uri'
 
 module Paperclip
   class UriAdapter < AbstractAdapter
+    attr_writer :content_type
+
     def initialize(target)
       @target = target
       @content = download_content
@@ -9,25 +11,41 @@ module Paperclip
       @tempfile = copy_to_tempfile(@content)
     end
 
-    attr_writer :content_type
-
     private
+
+    def cache_current_values
+      self.content_type = content_type_from_content || "text/html"
+
+      self.original_filename = filename_from_content_disposition ||
+                               filename_from_path || default_filename
+      @size = @content.size
+    end
+
+    def content_type_from_content
+      if @content.respond_to?(:content_type)
+        @content.content_type
+      end
+    end
+
+    def filename_from_content_disposition
+      if @content.meta.has_key?("content-disposition")
+        @content.meta["content-disposition"].
+          match(/filename="([^"]*)"/)[1]
+      end
+    end
+
+    def filename_from_path
+      @target.path.split("/").last
+    end
+
+    def default_filename
+      "index.html"
+    end
 
     def download_content
       options = { read_timeout: Paperclip.options[:read_timeout] }.compact
 
       open(@target, **options)
-    end
-
-    def cache_current_values
-      @original_filename = @target.path.split("/").last
-      @original_filename ||= "index.html"
-      self.original_filename = @original_filename.strip
-
-      @content_type = @content.content_type if @content.respond_to?(:content_type)
-      @content_type ||= "text/html"
-
-      @size = @content.size
     end
 
     def copy_to_tempfile(src)
