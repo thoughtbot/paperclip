5fb61847d992b8e193bb0e279cb00706a4cb686c
aboutqx <1004945442@qq.com>
2016-10-31 16:02:44 +0800

    Update geometry_detector_factory.rb

--- lib/paperclip/geometry_detector_factory.rb
+++ lib/paperclip/geometry_detector_factory.rb
@@ -17,7 +17,7 @@ module Paperclip
         orientation = Paperclip.options[:use_exif_orientation] ?
           "%[exif:orientation]" : "1"
         Paperclip.run(
-          Gem.win_platform?"magick identify":"identify",
+          (Gem.win_platform?)?"magick identify":"identify",
           "-format '%wx%h,#{orientation}' :file", {
             :file => "#{path}[0]"
           }, {
