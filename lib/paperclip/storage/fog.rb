04d52d8488f36e090514392d907d9e24cd4e4e5b
Georg Ledermann <mail@georg-ledermann.de>
2017-03-02 12:14:55 +0100

    Ruby 2.4.0 compatibility (doen't use Fixnum anymore)

--- lib/paperclip/storage/fog.rb
+++ lib/paperclip/storage/fog.rb
@@ -184,7 +184,7 @@ module Paperclip
       private
 
       def convert_time(time)
-        if time.is_a?(Fixnum)
+        if time.is_a?(0.class)
           time = Time.now + time
         end
         time
