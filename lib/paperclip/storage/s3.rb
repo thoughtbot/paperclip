92b3307ba33d744319aef2aec4fb51aeb2ea1a3f
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2018-01-29 19:22:48 -0800

    Docs: Provide example for s3_host_name (#2379)
    * Provide example for s3_host_name
    
    Gives an example, since not everyone knows the format required.
    
    * Shorten line per Hound comment

--- lib/paperclip/storage/s3.rb
+++ lib/paperclip/storage/s3.rb
@@ -94,7 +94,8 @@ module Paperclip
     #   to interpolate. Keys should be unique, like filenames, and despite the fact that
     #   S3 (strictly speaking) does not support directories, you can still use a / to
     #   separate parts of your file name.
-    # * +s3_host_name+: If you are using your bucket in Tokyo region etc, write host_name.
+    # * +s3_host_name+: If you are using your bucket in Tokyo region
+    #   etc, write host_name (e.g., 's3-ap-northeast-1.amazonaws.com').
     # * +s3_region+: For aws-sdk v2, s3_region is required.
     # * +s3_metadata+: These key/value pairs will be stored with the
     #   object.  This option works by prefixing each key with
