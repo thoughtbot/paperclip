0bd30e223f5f19bbc1f6737f8e6519719cda9b26
Michael Raidel <michael.raidel@atalanda.com>
2018-01-31 15:32:23 +0100

    fix fallback when symlink in AbstractAdapter#link_or_copy_file fails

--- lib/paperclip/io_adapters/abstract_adapter.rb
+++ lib/paperclip/io_adapters/abstract_adapter.rb
@@ -62,7 +62,9 @@ module Paperclip
       @destination.close
       @destination.open.binmode
     rescue Errno::EXDEV, Errno::EPERM, Errno::ENOENT, Errno::EEXIST => e
-      Paperclip.log("Link failed with #{e.message}; copying link #{src} to #{dest}")
+      Paperclip.log(
+        "Link failed with #{e.message}; copying link #{src} to #{dest}"
+      )
       FileUtils.cp(src, dest)
       @destination.close
       @destination.open.binmode
