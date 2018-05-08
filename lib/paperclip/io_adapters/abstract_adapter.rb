33ba2fd29245d9d64af399c6e58e33a5000c6f8e
Akihiko Odaki <akihiko.odaki.4i@stu.hosei.ac.jp>
2018-05-09 07:53:04 +0900

    Reopen file even when copying instead of linking (#2545)

--- lib/paperclip/io_adapters/abstract_adapter.rb
+++ lib/paperclip/io_adapters/abstract_adapter.rb
@@ -57,15 +57,16 @@ module Paperclip
     end
 
     def link_or_copy_file(src, dest)
-      Paperclip.log("Trying to link #{src} to #{dest}")
-      FileUtils.ln(src, dest, force: true) # overwrite existing
-      @destination.close
-      @destination.open.binmode
-    rescue Errno::EXDEV, Errno::EPERM, Errno::ENOENT, Errno::EEXIST => e
-      Paperclip.log(
-        "Link failed with #{e.message}; copying link #{src} to #{dest}"
-      )
-      FileUtils.cp(src, dest)
+      begin
+        Paperclip.log("Trying to link #{src} to #{dest}")
+        FileUtils.ln(src, dest, force: true) # overwrite existing
+      rescue Errno::EXDEV, Errno::EPERM, Errno::ENOENT, Errno::EEXIST => e
+        Paperclip.log(
+          "Link failed with #{e.message}; copying link #{src} to #{dest}"
+        )
+        FileUtils.cp(src, dest)
+      end
+
       @destination.close
       @destination.open.binmode
     end
