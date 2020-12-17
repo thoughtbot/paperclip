f0cfc4225b7238c861780afe9eb44ed0e15626cd
Gabriel Engel <gabrielfengel@gmail.com>
2020-12-17 14:34:05 -0300

    Update schema.rb

--- lib/paperclip/schema.rb
+++ lib/paperclip/schema.rb
@@ -51,7 +51,7 @@ module Paperclip
         attachment_names.each do |attachment_name|
           COLUMNS.each_pair do |column_name, column_type|
             column_options = options.merge(options[column_name.to_sym] || {})
-            column("#{attachment_name}_#{column_name}", column_type, column_options)
+            column("#{attachment_name}_#{column_name}", column_type, *column_options)
           end
         end
       end
